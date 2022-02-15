458c6ca634e58781b7ad7d2b2b914b58fac5e334
Bruno Cesar <brunocesar12386@gmail.com>
2022-02-15 20:05:22 -0300

    feat(accounts): create user controller

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import { stocksRoutes } from "./stoks.routes";
+import { usersRoutes } from "./users.routes";
 
 const router = Router();
 
 router.use("/stocks", stocksRoutes);
+router.use("/users", usersRoutes);
 
 export { router };
