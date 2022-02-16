27b558fe5a173290f4a7a4aa77e627c07bc1b06c
Bruno Cesar <brunocesar12386@gmail.com>
2022-02-15 21:05:03 -0300

    feat(accounts): create service sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 
+import { authenticateRoutes } from "./authenticate.routes";
 import { stocksRoutes } from "./stoks.routes";
 import { usersRoutes } from "./users.routes";
 
@@ -7,5 +8,6 @@ const router = Router();
 
 router.use("/stocks", stocksRoutes);
 router.use("/users", usersRoutes);
+router.use(authenticateRoutes);
 
 export { router };
