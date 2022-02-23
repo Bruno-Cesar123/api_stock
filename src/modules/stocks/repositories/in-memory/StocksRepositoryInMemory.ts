aee7c705af4092574a1a03289a3d83ab9b0d9538
Bruno Cesar <brunocesar12386@gmail.com>
2022-02-22 21:17:07 -0300

    test(accounts): implement createUserUseCase test

---
+++ src/modules/stocks/repositories/in-memory/StocksRepositoryInMemory.ts
@@ -0,0 +1,49 @@
+import { Stock } from "../../entities/Stock";
+import { ICreateStockDTO, IStocksRepository } from "../IStocksRepository";
+
+class StocksRepositoryInMemory implements IStocksRepository {
+  stocks: Stock[] = [];
+
+  async findByName(name: string): Promise<Stock> {
+    const stock = this.stocks.find(stock => stock.name === name);
+    return stock;
+  }
+
+  async findById(id: string): Promise<Stock> {
+    const stock = this.stocks.find(stock => stock.id === id);
+    return stock;
+  }
+
+  async list(): Promise<Stock[]> {
+    const all = this.stocks;
+
+    return all;
+  }
+
+  async create({
+    name,
+    type,
+    average_price,
+    sales_price,
+    quantity,
+  }: ICreateStockDTO): Promise<void> {
+    const stock = new Stock();
+
+    Object.assign(stock, {
+      name,
+      type,
+      average_price,
+      sales_price,
+      quantity,
+    });
+
+    this.stocks.push(stock);
+  }
+  async deleteStock(id: string): Promise<void> {
+    this.stocks = this.stocks.filter(stock => {
+      return stock.id !== id;
+    });
+  }
+}
+
+export { StocksRepositoryInMemory };
--- src/modules/stocks/useCases/createStock/CreateStockUseCase.spec.ts
+++ src/modules/stocks/useCases/createStock/CreateStockUseCase.spec.ts
@@ -1,9 +1,63 @@
-describe("CreateStock", () => {
-  it("espero que 2 + 2 = 4", () => {
-    const soma = 2 + 2;
+import { AppError } from "../../../../errors/AppError";
+import { StocksRepositoryInMemory } from "../../repositories/in-memory/StocksRepositoryInMemory";
+import { CreateStockUseCase } from "./CreateStockUseCase";
 
-    const resultado = 4;
+let createStockUseCase: CreateStockUseCase;
+let stocksRepositoryInMemory: StocksRepositoryInMemory;
 
-    expect(soma).toBe(resultado);
+describe("Create Stock", () => {
+  beforeEach(() => {
+    stocksRepositoryInMemory = new StocksRepositoryInMemory();
+    createStockUseCase = new CreateStockUseCase(stocksRepositoryInMemory);
+  });
+
+  it("should be able to create a new stock", async () => {
+    const stock = {
+      name: "Stock Test",
+      type: "Stock Type Test",
+      average_price: 2000,
+      sales_price: 4000,
+      quantity: 100,
+    };
+
+    await createStockUseCase.execute({
+      name: stock.name,
+      type: stock.type,
+      average_price: stock.average_price,
+      sales_price: stock.sales_price,
+      quantity: stock.quantity,
+    });
+
+    const stockCreated = await stocksRepositoryInMemory.findByName(stock.name);
+
+    expect(stockCreated).toHaveProperty("id");
+  });
+
+  it("should not be able to create a new stock with name exists", async () => {
+    expect(async () => {
+      const stock = {
+        name: "Stock Test",
+        type: "Stock Type Test",
+        average_price: 2000,
+        sales_price: 4000,
+        quantity: 100,
+      };
+
+      await createStockUseCase.execute({
+        name: stock.name,
+        type: stock.type,
+        average_price: stock.average_price,
+        sales_price: stock.sales_price,
+        quantity: stock.quantity,
+      });
+
+      await createStockUseCase.execute({
+        name: stock.name,
+        type: stock.type,
+        average_price: stock.average_price,
+        sales_price: stock.sales_price,
+        quantity: stock.quantity,
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
